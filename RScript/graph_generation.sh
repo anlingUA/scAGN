0199214ca6df8f1bc1b691c451eaa91ce2f6b521
Rahul Bhadani <rahulbhadani@email.arizona.edu>
2022-04-16 15:32:06 -0700

    Commit

--- RScript/graph_generation.sh
+++ RScript/graph_generation.sh
@@ -1,34 +1,22 @@
 #!/bin/bash
-#Rscript generate_data_for_CCA.R -d 1 -f scData -s 0.75 -t 50 &> output1.log &
-#Rscript generate_data_for_CCA.R -d 2 -f scData -s 0.75 -t 50 &> output2.log &
-#Rscript generate_data_for_CCA.R -d 3 -f scData -s 0.75 -t 50 &> output3.log &
-#Rscript generate_data_for_CCA.R -d 4 -f scData -s 0.75 -t 50 &> output4.log &
-#Rscript generate_data_for_CCA.R -d 5 -f scData -s 0.75 -t 50 &> output5.log &
-#Rscript generate_data_for_CCA.R -d 6 -f scData -s 0.75 -t 50 &> output6.log &
-#Rscript generate_data_for_CCA.R -d 8 -f scData -s 0.75 -t 50 &> output8.log &
-#Rscript generate_data_for_CCA.R -d 9 -f scData -s 0.75 -t 10 &> output9.log &
-#Rscript generate_data_for_CCA.R -d 21 -f scData -s 0.75 -t 10 &> output21.log &
-#Rscript generate_data_for_CCA.R -d 22 -f scData -s 0.75 -t 10 &> output22.log &
-#Rscript generate_data_for_CCA.R -d 23 -f scData -s 0.75 -t 10 &> output23.log &
-#Rscript generate_data_for_CCA.R -d 24 -f scData -s 0.75 -t 10 &> output24.log &
-#Rscript generate_data_for_CCA.R -d 25 -f scData -s 0.75 -t 10 &> output25.log &
-#Rscript generate_data_for_CCA.R -d 26 -f scData -s 0.75 -t 10 &> output26.log &
+Rscript generate_data_for_CCA.R -d 1 -f scData -s 0.75 -t 50 &> output1.log &
+Rscript generate_data_for_CCA.R -d 2 -f scData -s 0.75 -t 50 &> output2.log &
+Rscript generate_data_for_CCA.R -d 3 -f scData -s 0.75 -t 50 &> output3.log &
+Rscript generate_data_for_CCA.R -d 4 -f scData -s 0.75 -t 50 &> output4.log &
+Rscript generate_data_for_CCA.R -d 5 -f scData -s 0.75 -t 50 &> output5.log &
+Rscript generate_data_for_CCA.R -d 6 -f scData -s 0.75 -t 50 &> output6.log &
+Rscript generate_data_for_CCA.R -d 8 -f scData -s 0.75 -t 50 &> output8.log &
+Rscript generate_data_for_CCA.R -d 9 -f scData -s 0.75 -t 10 &> output9.log &
+Rscript generate_data_for_CCA.R -d 21 -f scData -s 0.75 -t 10 &> output21.log &
+Rscript generate_data_for_CCA.R -d 22 -f scData -s 0.75 -t 10 &> output22.log &
+Rscript generate_data_for_CCA.R -d 23 -f scData -s 0.75 -t 10 &> output23.log &
+Rscript generate_data_for_CCA.R -d 24 -f scData -s 0.75 -t 10 &> output24.log &
+Rscript generate_data_for_CCA.R -d 25 -f scData -s 0.75 -t 10 &> output25.log &
+Rscript generate_data_for_CCA.R -d 26 -f scData -s 0.75 -t 10 &> output26.log &
 
-#for i in {26..57}
-#do
-#   Rscript generate_data_for_CCA.R -d $i -f scData -s 0.75 -t 10 &> output$i.log &
-#done
-#Rscript generate_data_for_CCA.R -d 28 -f scData -s 0.75 -t 10 &> graph_gen_output_28.log
-#Rscript generate_data_for_CCA.R -d 29 -f scData -s 0.75 -t 10 &> graph_gen_output_29.log
-#Rscript generate_data_for_CCA.R -d 31 -f scData -s 0.75 -t 10 &> graph_gen_output_31.log
-#Rscript generate_data_for_CCA.R -d 32 -f scData -s 0.75 -t 10 &> graph_gen_output_32.log
-#Rscript generate_data_for_CCA.R -d 33 -f scData -s 0.75 -t 10 &> graph_gen_output_33.log
-#Rscript generate_data_for_CCA.R -d 36 -f scData -s 0.75 -t 10 &> graph_gen_output_36.log
-#Rscript generate_data_for_CCA.R -d 37 -f scData -s 0.75 -t 10 &> graph_gen_output_37.log
-#Rscript generate_data_for_CCA.R -d 38 -f scData -s 0.75 -t 10 &> graph_gen_output_38.log
-Rscript generate_data_for_CCA.R -d 43 -f scData -s 0.75 -t 10 &> graph_gen_output_43.log
-Rscript generate_data_for_CCA.R -d 49 -f scData -s 0.75 -t 10 &> graph_gen_output_49.log
-Rscript generate_data_for_CCA.R -d 53 -f scData -s 0.75 -t 10 &> graph_gen_output_53.log
-Rscript generate_data_for_CCA.R -d 57 -f scData -s 0.75 -t 10 &> graph_gen_output_57.log
+for i in {26..57}
+do
+   Rscript generate_data_for_CCA.R -d $i -f scData -s 0.75 -t 10 &> output$i.log &
+done
 
 
